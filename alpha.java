517fa6e5ee5985dac1b9f9e08db691dff0a2e037
Ashwini14092003 <152679212+Ashwini14092003@users.noreply.github.com>
2023-12-24 10:41:06 +0530

    Add files via upload

---
+++ alpha.java
@@ -0,0 +1,27 @@
+public class alpha
+{
+public static void main(String args[])
+{
+char firstchar='a';
+char lastchar='z';
+for(char startchar=firstchar;startchar<=lastchar;startchar++)
+{
+system.out.print(startchar+"");
+}
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
